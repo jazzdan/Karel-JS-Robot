c882fadb22d2fbc653ea22717a885655f3dfa10b
Dan Miller <dan@danielsmiller.com>
2011-12-23 18:42:40 -0500

    Fixed some layout issues but codemirror is acting really funky and not inserting stuff into the textarea like it should.

--- js/script.js
+++ js/script.js
@@ -1,26 +1,13 @@
 /* Author: 
 
-*/
+*/            
+var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+                                          value: "function myScript(){return 100;}\n",
+                                          path:  "mode",
+                                          mode:  "javascript",
+                                          theme: "monokai"
+                                          });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+    lineNumbers: true
+  });*/
